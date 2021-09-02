b38bc8dfdc532bbdbdfba4290e4e3b7ac8f22c5c
Patras Catalina <patrascatalina@gmail.com>
2021-09-02 09:48:17 +0300

    fixed: notification message

--- routes/auth.js
+++ routes/auth.js
@@ -10,14 +10,11 @@ const tokenSecret = "my-token-secret"
 const middleware = require('../middlewares')
 
 router.post('/signup', (req, res) => {
-    console.log("merge22")
-    console.log(req.body)
     bcrypt.hash(req.body.password, rounds, (error, hash) => {
         if (error) 
         res.status(501).json(error)
         else {
             const newUser =  User({username: req.body.username,email: req.body.email, password: hash})
-            console.log(newUser)
             newUser.save()
                 .then(user => {
                     res.status(200).json({token: generateToken(user)})
@@ -32,12 +29,12 @@ router.post('/signup', (req, res) => {
 router.post('/signin', (req, res) => {
     User.findOne({email: req.body.email})
     .then(user => {
-        if(!user) res.status(404).json({error: 'no user with that email found'})
+        if(!user) res.status(404).json({error: 'The email adress or the password do not match !'})
         else {
             bcrypt.compare(req.body.password, user.password, (error, match) => {
                 if (error) res.status(500).json(error)
                 else if (match) res.status(200).json({id:user.id, username:user.username, email: user.email, password: user.password, token: generateToken(user), time: new Date()})
-                else res.status(403).json({error: 'passwords do not match'})
+                else res.status(403).json({error: 'The email adress or the password do not match !'})
             })
         }
     })
@@ -47,20 +44,20 @@ router.post('/signin', (req, res) => {
 });
 
 router.post('/signout', (req, res) => {
-    User.findOne({email: req.body.email})
-    .then(user => {
-        if(!user) res.status(404).json({error: 'no user with that email found'})
-        else {
-            bcrypt.compare(req.body.password, user.password, (error, match) => {
-                if (error) res.status(500).json(error)
-                else if (match) res.status(200).json({email: user.email, password: user.password})
-                else res.status(403).json({error: 'passwords do not match'})
-            })
-        }
-    })
-    .catch(error => {
-        res.status(500).json(error)
-    })
+    // User.findOne({email: req.body.email})
+    // .then(user => {
+    //     if(!user) res.status(404).json({error: 'no user with that email found'})
+    //     else {
+    //         bcrypt.compare(req.body.password, user.password, (error, match) => {
+    //             if (error) res.status(500).json(error)
+    //             else if (match) res.status(200).json({email: user.email, password: user.password})
+    //             else res.status(403).json({error: 'passwords do not match'})
+    //         })
+    //     }
+    // })
+    // .catch(error => {
+    //     res.status(500).json(error)
+    // })
 });
 router.get('/jwt-test', middleware.verify , (req, res) => {
     res.status(200).json(req.user)
